ebb3755c510329b4c38b833dc8040437255dcf7c
Jason Cabezuela <cabezuela@syntexsoft.com>
2025-04-01 20:01:10 +0200

    Add more env-vars and remove rcon-pw
    Add more env-vars and add descriptions

--- docker/minecraft/entrypoint.sh
+++ docker/minecraft/entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-
-if [ -f /opt/stxsoft/config/server.properties ]; then
-	echo "🔧 Copying custom server.properties"
-	cp /opt/stxsoft/config/server.properties /data/server.properties
-	chown 1000:1000 /data/server.properties
-	chmod 644 /data/server.properties
+if [ -n "${CUSTOM_CONFIG}" ]; then
+    echo "CUSTOM_CONFIG is set"
+    if [ -f "/opt/stxsoft/config/${CUSTOM_CONFIG}" ]; then
+        echo "🔧 Copying custom server config to '/data/server.properties'"
+    	cp "/opt/stxsoft/config/${CUSTOM_CONFIG}" /data/server.properties
+	    chown 1000:1000 /data/server.properties
+    	chmod 644 /data/server.properties
+    fi
+else
+    echo "CUSTOM_CONFIG is not set, server will use default configuration!"
 fi
 
 exec /start
